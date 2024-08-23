df94811d354501b850389ba4a5011802bc4ddc1b
Dovydas M <dovydas.m@gmail.com>
2024-08-24 02:26:59 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestArtifact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
